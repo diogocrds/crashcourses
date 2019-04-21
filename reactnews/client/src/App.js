5f95ab49725004592e172e5586d67599ae94105f
Diogo J. Cardoso <diogoopequeno@hotmail.com>
2019-04-21 15:55:15 -0300

    basic rect app

--- reactnews/client/src/App.js
+++ reactnews/client/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Home from './components/layouts/Home';
+import About from './components/layouts/About';
+import Layout from './components/layouts/Layout';
+import NewsArticle from './components/containers/NewsArticle';
+import store from './store/store';
+import './App.css';
 
 class App extends Component {
   render() {
-    return <Home />;
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Layout>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/news/:id' component={NewsArticle} />
+          </Layout>
+        </BrowserRouter>
+      </Provider>
+    );
   }
 }
 
--- reactnews/client/src/components/layouts/Home.js
+++ reactnews/client/src/components/layouts/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import News from '../containers/News';
 
 class Home extends Component {
   render() {
